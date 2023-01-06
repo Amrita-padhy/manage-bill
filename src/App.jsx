63617b83359fc7825a7b03d3f6e6deaef9daf638
bibhu-padhy <bibhupadhy140@gmail.com>
2023-01-06 16:15:24 +0530

    changes related to the route setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,5 @@
-import { useState } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-
-import "./App.css";
-import OnBoardPage from "./pages/OnBoardPage";
-import SignInPage from "./pages/auth/SignInPage";
-import SignUpPage from "./pages/auth/SingUpPage";
-import ForgetPasswordPage from "./pages/auth/ForgetPasswordPage";
-import ForgetPasswordEmailSentPage from "./pages/auth/ForgetPasswordEmailSentPage";
-import SetNewPasswordPage from "./pages/auth/SetNewPasswordPage";
-import ResetPasswordPage from "./pages/auth/ResetPasswordPage";
-import PopUp from "./components/PopUp";
-
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/sign-in-page" element={<SignInPage />} />
-        <Route path="/sign-up-page" element={<SignUpPage />} />
-        <Route path="/onboard-page" element={<OnBoardPage />} />
-        <Route path="/forget-password-page" element={<ForgetPasswordPage />} />
-        <Route
-          path="/forget-password-email-sent-page"
-          element={<ForgetPasswordEmailSentPage />}
-        />
-        <Route path="/set-new-password-page" element={<SetNewPasswordPage />} />
-        <Route path="/reset-password-page" element={<ResetPasswordPage />} />
-        <Route path="/modal" element={<PopUp />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <></>;
 }
 
 export default App;
---
+++ src/boot/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCGr0nSFpJmcDZcczCb3U6yfEYGdcUZcPk",
+  authDomain: "manage-bill-1af1e.firebaseapp.com",
+  projectId: "manage-bill-1af1e",
+  storageBucket: "manage-bill-1af1e.appspot.com",
+  messagingSenderId: "801917484700",
+  appId: "1:801917484700:web:8ce3fbd7108139883a3759",
+  measurementId: "G-PGLEVEEW85",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
