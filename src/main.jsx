4134db8477d21af6dad5a198d2e7bb27339a0706
bibhu-padhy <bibhupadhy140@gmail.com>
2023-02-06 17:53:40 +0530

    auto populate personal info and added general layout

--- src/main.jsx
+++ src/main.jsx
@@ -2,26 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ThemeProvider } from "@mui/material/styles";
+import AuthContextProvider from "./context/authContext";
+import store from "@/store/store.js";
+import { customTheme } from "./styles/colorPalette";
+import { CssBaseline } from "@mui/material";
+import { Provider } from "react-redux";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 import "./index.css";
 
 import AuthLayout from "./layout/AuthLayout";
+import GeneralLayout from "./layout/GeneralLayout";
 import SignIn from "./pages/auth/SignInPage";
 import SignUp from "./pages/auth/SingUpPage";
+import OnBoardPage from "./pages/OnBoardPage";
 import ForgetPasswordPage from "./pages/auth/ForgetPasswordPage";
 import ForgetPasswordEmailSentPage from "./pages/auth/ForgetPasswordEmailSentPage";
 import ResetPasswordPage from "./pages/auth/ResetPasswordPage";
-
 import SetNewPasswordPage from "./pages/auth/SetNewPasswordPage";
-import OnBoardPage from "./pages/OnBoardPage";
-import { customTheme } from "./styles/colorPalette";
-import { CssBaseline } from "@mui/material";
-import store from "@/store/store.js";
-import { Provider } from "react-redux";
-import { QueryClient, QueryClientProvider } from "react-query";
-import AuthContextProvider from "./context/authContext";
-import PaymentDetailsCard from "./components/onboardPage/plan&PaymentDetails/PaymentDetailsCard";
-import ProfileDetailsCard from "./components/onboardPage/profile&companyDetails/ProfileDetailsCard";
 
 const queryClient = new QueryClient();
 
@@ -39,7 +37,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
                 <Route path="/signup" element={<SignUp />} />
                 <Route path="/onboard" element={<OnBoardPage />}></Route>
               </Route>
-              <Route path="/main" element={GeneralLayout}></Route>
+              <Route path="/main" element={<GeneralLayout />} />
             </Routes>
           </ThemeProvider>
         </AuthContextProvider>
